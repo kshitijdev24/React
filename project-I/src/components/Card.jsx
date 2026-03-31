293e99f3e12c0982212c3eb13a5ffbbac47eee53
kshitijdev24 <kshitij.chetan030174@gmail.com>
2026-04-01 03:59:37 +0530

    feat: add Card component and styles, integrate SVG assets for React and Vite
    - Created a new Card component for displaying job listings with props for brand logo, company name, date posted, position, tags, pay, and location.
    - Added index.css for global styles including card layout and responsive design.
    - Introduced SVG assets for React and Vite logos.
    - Set up main entry point with React and Vite configuration.

---
+++ project-I/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img
+        src={props.img}
+        alt=""
+      />
+          <h1>{ props.user}</h1>
+      <p>
+       Inservice since:{props.Inservice}
+      </p>
+      <button>View Profile</button>
+    </div>
+  );
+};
+
+export default Card;
